f6c2d5121341d675ea2570333fbd5476ecc0c715
Michael Manning <michaelmanning390@gmail.com>
2019-05-20 22:33:06 -0700

    Created spoon.sh because I hated creating new script files.

---
+++ spoon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Takes a CamelCase class name to generate a lua class for DinoDeck engine
+NAME=$1
+PROJECT="/d/Railalis/Documents/Code/lua/luaRpg"
+SCRIPTS="scripts"
+DEP="Dependencies.lua"
+FILE=$PROJECT/$SCRIPTS/$NAME.lua
+
+# echo "Creating $NAME.lua file.."
+touch $FILE
+echo "$NAME = {}" >> $FILE
+echo "$NAME.__index = $NAME" >> $FILE
+
+# echo "Adding to manifest.."
+tac $PROJECT/manifest.lua | sed "5 i \\\t\t['$NAME.lua'] = { path = '$SCRIPTS/$NAME.lua' }," | tac > $PROJECT/manifest.lua
+
+# echo "Adding to $DEP.."
+tac $PROJECT/$SCRIPTS/$DEP | sed "2 i \\\t\"$NAME.lua\"," | tac > $PROJECT/$SCRIPTS/$DEP
+
+# now open it
+subl $FILE
