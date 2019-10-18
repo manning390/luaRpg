18b572039375c795d03bcb3e77fb053f35352b4f
Michael Manning <michaelmanning390@gmail.com>
2019-10-18 00:05:46 -0700

    Updated spoon again.

--- spoon.sh
+++ spoon.sh
@@ -3,19 +3,30 @@
 PROJECT='/d/Railalis/Documents/Code/lua/luaRpg'
 SCRIPTS='scripts'
 DEP='Dependencies.lua'
-NAME=$1
+# wut? Just go with it
+NAME=${1%\.lua}
+CLASS=${NAME##*\/}
 FILE=$PROJECT/$SCRIPTS/$NAME.lua
+DIRS=${FILE%$CLASS\.lua}
+
+# Maybe one day
+# echo "Tarnishing the spoons.."
+# unalias spoon 2>/dev/null
+# alias spoon="$PROJECT/spoon.sh"
+
+# echo "Creating directories..?"
+mkdir -p $DIRS
 
 # echo "Creating $NAME.lua file.."
 touch $FILE
-echo "$NAME = {}" >> $FILE
-echo "$NAME.__index = $NAME" >> $FILE
+echo "$CLASS = {}" >> $FILE
+echo "$CLASS.__index = $CLASS" >> $FILE
 
 # echo "Adding to manifest.."
-sed -i "$(expr $(wc -l < $PROJECT/manifest.lua) - 1)i\\\t\t['$NAME.lua'] = { path = '$SCRIPTS/$NAME.lua' }," $PROJECT/manifest.lua
+sed -i "$(expr $(wc -l < $PROJECT/manifest.lua) - 1)i\\\t\t['$CLASS.lua'] = { path = '$SCRIPTS/$NAME.lua' }," $PROJECT/manifest.lua
 
 # echo "Adding to $DEP.."
-sed -i "$(wc -l < $PROJECT/$SCRIPTS/$DEP)i\\\t\"$NAME.lua\"," $PROJECT/$SCRIPTS/$DEP
+sed -i "$(wc -l < $PROJECT/$SCRIPTS/$DEP)i\\\t\"$CLASS.lua\"," $PROJECT/$SCRIPTS/$DEP
 
 # now open it
 subl $FILE
